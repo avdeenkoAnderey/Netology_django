90bb72dfd42e581d53666c3e8d3cfd5f240a313e
ANDREY AVDEENKO <avdey.1996@mail.ru>
2026-06-08 21:56:38 +0300

    убрал дублирование books

--- databases/models_list_displaying/main/urls.py
+++ databases/models_list_displaying/main/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', include('books.urls')),
+    path('', include('books.urls')),#http://127.0.0.1:8000/books/
 ]
