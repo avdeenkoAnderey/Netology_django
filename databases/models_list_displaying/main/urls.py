b2d7cd59d997c57a05e31fab69e2ebfa23e14465
ANDREY AVDEENKO <avdey.1996@mail.ru>
2026-06-08 20:41:34 +0300

    add include

--- databases/models_list_displaying/main/urls.py
+++ databases/models_list_displaying/main/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
-from books.views import books_view
 
 urlpatterns = [
-    path('', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('',include('books.urls')),
+    path('books/', include('books.urls')),
 ]
