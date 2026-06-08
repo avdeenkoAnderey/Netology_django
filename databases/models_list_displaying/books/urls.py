e82fd84e147db2609120ad45cfa5d8f04a28e7c9
ANDREY AVDEENKO <avdey.1996@mail.ru>
2026-06-08 20:47:08 +0300

    corr

--- databases/models_list_displaying/books/urls.py
+++ databases/models_list_displaying/books/urls.py
@@ -3,6 +3,5 @@ from books.views import books_view,books_by_date
 
 urlpatterns = [
     path('books/', books_view, name='books'),
-    path('books/<str:date_str>',books_by_date, name='books_by_date')
-
+    path('books/<str:date_str>',books_by_date, name='books_by_date'),
 ]
